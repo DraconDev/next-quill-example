20c756f7f43b52531852aeecd346d6a75bdf6ae3
DraconDev <dracsharp@gmail.com>
2023-12-17 21:11:51 +0000

    upd to use dynamic import

--- components/Quill/QuillBox.tsx
+++ components/Quill/QuillBox.tsx
@@ -1,21 +1,38 @@
 "use client";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css"; // or 'quill.bubble.css' for bubble theme
+
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+
+import "react-quill/dist/quill.snow.css";
+// or 'quill.bubble.css' for bubble theme
 export interface IAppProps {
     userText: string;
     setUserText: (text: string) => void;
 }
 
 export function QuillBox({ userText, setUserText }: IAppProps) {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(true);
+    }, []);
+
+    const ReactQuill = dynamic(() => import("react-quill"), {
+        ssr: false,
+        loading: () => <p>Loading...</p>,
+    });
+
     return (
-        <div className="  w-full h-[320px] mb-10 bg-white text-black ">
-            <ReactQuill
-                theme="snow"
-                value={userText}
-                onChange={setUserText}
-                // style={{ height: "285px" }}
-                className=" rounded-lg h-[277px] "
-            />
-        </div>
+        loaded && (
+            <div className="  w-full h-[320px] mb-10 bg-white text-black ">
+                <ReactQuill
+                    theme="snow"
+                    value={userText}
+                    onChange={setUserText}
+                    // style={{ height: "285px" }}
+                    className=" rounded-lg h-[277px] "
+                />
+            </div>
+        )
     );
 }
