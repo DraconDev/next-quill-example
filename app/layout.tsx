be91d9dd8fdc7bd6c5bcef96c7c8eeac8e385eaf
DraconDev <dracsharp@gmail.com>
2023-12-17 05:12:35 +0000

    basic style

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={`{inter.className} min-h-screen`}>{children}</body>
+            <body className={`${inter.className} min-h-screen text-secondary`}>
+                {children}
+            </body>
         </html>
     );
 }
